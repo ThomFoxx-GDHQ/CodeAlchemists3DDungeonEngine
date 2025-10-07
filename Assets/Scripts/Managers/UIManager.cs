3c946b74313da77067b2a27e3a963399b1280219
Thomas Kesler <thom@gamedevhq.com>
2025-10-07 13:42:18 -1000

    Day 32a: Save to Rescue old Party Menu

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,6 +1,22 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class UIManager : MonoBehaviour
 {
-    //Main Control System for UI. For Tuesday.
+    [SerializeField] GameObject _mainMenuCanvas;
+
+    [SerializeField] InputActionReference _mainMenuAction;
+
+    private void OnEnable()
+    {
+        _mainMenuAction.action.performed += Action_performed;
+
+    }
+
+    private void Action_performed(InputAction.CallbackContext obj)
+    {
+        _mainMenuCanvas.SetActive(!_mainMenuCanvas.activeInHierarchy);
+    }
 }
